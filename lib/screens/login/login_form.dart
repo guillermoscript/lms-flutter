1915bcfd7bdc8bf86af110d1918c386165f46422
Ángel Afonso <angelafonso60@gmail.com>
2024-05-30 21:28:00 -0400

    added login functionality

---
+++ lib/screens/login/login_form.dart
@@ -0,0 +1,97 @@
+import 'package:flutter/material.dart';
+
+final buttonStlye = ButtonStyle(
+  shape: WidgetStateProperty.all<OutlinedBorder>(
+    RoundedRectangleBorder(
+      borderRadius: BorderRadius.circular(8),
+    ),
+  ),
+);
+
+class LoginForm extends StatefulWidget {
+  final Function(String email, String password) onSubmit;
+  final bool isLoading;
+
+  const LoginForm({super.key, required this.onSubmit, this.isLoading = false});
+
+  @override
+  LoginFormState createState() => LoginFormState();
+}
+
+class LoginFormState extends State<LoginForm> {
+  final _formKey = GlobalKey<FormState>();
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: _formKey,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          TextFormField(
+            enabled: !widget.isLoading,
+            controller: _emailController,
+            decoration: const InputDecoration(
+              labelText: 'Email',
+              border: OutlineInputBorder(),
+            ),
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return 'Please enter your email';
+              }
+              return null;
+            },
+          ),
+          const SizedBox(height: 16),
+          TextFormField(
+            enabled: !widget.isLoading,
+            controller: _passwordController,
+            obscureText: true,
+            decoration: const InputDecoration(
+              labelText: 'Password',
+              border: OutlineInputBorder(),
+            ),
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return 'Please enter your password';
+              }
+              return null;
+            },
+          ),
+          const SizedBox(height: 16),
+          FilledButton(
+            onPressed: () {
+              if (_formKey.currentState!.validate()) {
+                widget.onSubmit(
+                  _emailController.text,
+                  _passwordController.text,
+                );
+              }
+            },
+            style: buttonStlye,
+            child: const Text('Login'),
+          ),
+          const SizedBox(height: 16),
+          TextButton(
+            onPressed: () {},
+            style: buttonStlye,
+            child: const Text('Forgot your password?'),
+          ),
+          TextButton(
+            onPressed: () {},
+            style: ButtonStyle(
+              shape: WidgetStateProperty.all<OutlinedBorder>(
+                RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(8),
+                ),
+              ),
+            ),
+            child: const Text("Don't have an account? Sign up"),
+          ),
+        ],
+      ),
+    );
+  }
+}
